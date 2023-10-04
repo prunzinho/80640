7d6bbe1c590c82682ba69d65341f17041be774eb
prunzinho <cdgn_17@hotmail.com>
2023-10-04 14:00:45 -0600

    practica07

--- practica07.js
+++ practica07.js
@@ -1,14 +1,16 @@
 
-//tipos de funciones y back tilde
+//tipos de funciones y back tilde, interpolacion, Dom, uso de platinllas para crear un componente dinamico
+
 const formulario = document.getElementById("formulario")
 const miFieldSet = (legend, txt1, txt2) => {
     return `
     <legend>${legend}</legend>
-    <label for="user">${txt1}</label>
-    <input type="text" id="user" name="user">
-    <label for="correo">${txt2}</label>
-    <input type="text" id="correo" name="correo"> 
+    <label for=${txt1}>${txt1}</label>
+    <input type="text" id=${txt1} name="user">
+    <label for=${txt2}>${txt2}</label>
+    <input type="text" id=${txt2} name="correo"> 
     `
 }
 
-formulario.innerHTML = miFieldSet
+formulario.innerHTML = miFieldSet("Informacion Personal", "Nombre", "Correo")
+formulario.innerHTML += miFieldSet("Informacion Direccion", "Direccion", "Ciudad")
