3973f48f2135b1a38bd19f830a6dba27b166c3cd
prunzinho <cdgn_17@hotmail.com>
2023-10-19 12:55:15 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -1,7 +1,19 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        get("/",
+                (request, response) -> "<h1> Hola Mundo </h1>");
+        get("/ruta",
+                (request, response) -> "<h1> Que onda Mundo </h1>");
+        get("/ruta2",
+                (request, response) -> "<h1> Adios Mundo </h1>");
+        get("/ruta3",
+                (request, response) -> "{'alumno':'jhon','matricula':'s2200','carrera':'TC'}");
+
     }
+
 }
