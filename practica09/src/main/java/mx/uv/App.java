d2354d86a5c3a733489f44330ce4b65bc0d5b962
prunzinho <cdgn_17@hotmail.com>
2023-11-10 07:19:03 -0600

    practica09

--- practica09/src/main/java/mx/uv/App.java
+++ practica09/src/main/java/mx/uv/App.java
@@ -3,17 +3,43 @@ package mx.uv;
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        get("/",
-                (request, response) -> "<h1> Hola Mundo </h1>");
-        get("/ruta",
-                (request, response) -> "<h1> Que onda Mundo </h1>");
-        get("/ruta2",
-                (request, response) -> "<h1> Adios Mundo </h1>");
-        get("/ruta3",
-                (request, response) -> "{'alumno':'jhon','matricula':'s2200','carrera':'TC'}");
+        public static void main(String[] args) {
+                System.out.println("Hola mundo");
 
-    }
+                // fuente:https://gist.github.com/saeidzebardast/e375b7d17be3e0f4dddf
+
+                options("/*", (request, response) -> {
+
+                        String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+
+                        if (accessControlRequestHeaders != null) {
+
+                                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+
+                        }
+
+                        String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+
+                        if (accessControlRequestMethod != null) {
+
+                                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+
+                        }
+
+                        return "OK";
+
+                });
+
+                before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
+                get("/",
+                                (request, response) -> "<h1> Hola Mundo </h1>");
+                get("/ruta",
+                                (request, response) -> "<h1> Que onda Mundo </h1>");
+                get("/ruta2",
+                                (request, response) -> "<h1> Adios Mundo </h1>");
+                get("/ruta3",
+                                (request, response) -> "{\"alumno\": \"jhon\",\"matricula\":\"s2200\",\"carrera\":\"TC\"}");
+        }
 
 }
